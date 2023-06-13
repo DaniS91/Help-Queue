77260f6f361d744106a68917bebeffd385030115
DaniS91 <114887061+DaniS91@users.noreply.github.com>
2023-06-12 18:43:10 -0700

    start adding time elapsed to tickets

--- src/components/TicketList.js
+++ src/components/TicketList.js
@@ -5,17 +5,18 @@ import PropTypes from "prop-types";
 function TicketList(props){
   return (
     <React.Fragment>
-        <hr/>
-        {Object.values.map((ticket) =>
-          <Ticket 
-            whenTicketClicked = { props.onTicketSelection }
-            names={ticket.names}
-            location={ticket.location}
-            issue={ticket.issue}
-            id = {ticket.id}
-            key={ticket.id}/>
-        )}
-      </React.Fragment>
+      <hr/>
+      {Object.values(props.ticketList).map((ticket) => {
+        return <Ticket
+          whenTicketClicked = { props.onTicketSelection }
+          names={ticket.names}
+          location={ticket.location}
+          issue={ticket.issue}
+          formattedWaitTime={ticket.formattedWaitTime}
+          id={ticket.id}
+          key={ticket.id}/>
+  })}
+    </React.Fragment>
   );
 }
 
