6ac0f05bee0aabe05f47f0558caf70965be06882
DaniS91 <114887061+DaniS91@users.noreply.github.com>
2023-06-25 15:59:47 -0700

    add links to header from routes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,19 @@
 import React from "react";
 import ticketsImage from "./../img/tickets-image.png"
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <React.Fragment>
-      <h1><img src={ticketsImage} alt="a ticket" height="32"/>Help Queue</h1>
+      <h1><img src={ticketsImage} alt="a ticket" height="32"/> Help Queue</h1>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/sign-in">Sign In</Link>
+        </li>
+      </ul>
     </React.Fragment>
   );
 }
